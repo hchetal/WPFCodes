2a0b49eafa2392da311dea61fd274278c0e6b41e
Himanshu-HP\Himanshu <himanshu.chetal@outlook.com>
2017-03-26 21:25:52 +0800

    Revert "Added to Branch 4"
    This reverts commit 1cf5f35c2ada2e8a24edce9239e0a64ad23571f3.

--- WPFEvent/WPFEvent/MainWindow.xaml.cs
+++ WPFEvent/WPFEvent/MainWindow.xaml.cs
@@ -15,7 +15,9 @@ using System.Windows.Shapes;
 
 namespace WPFEvent
 {
-
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
